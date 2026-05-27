fc0e959151a1d008cb2011a87d51d0e34bda7e95
Frappe <frappe@erpnext-live.lxd>
2026-05-27 19:02:31 +0100

    Add product gallery doctypes

---
+++ webshop_image_gallery/webshop_image_gallery/doctype/product_gallery/product_gallery.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, WIPRA SOFT Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Product Gallery", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ webshop_image_gallery/webshop_image_gallery/doctype/product_gallery/test_product_gallery.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, WIPRA SOFT Ltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProductGallery(FrappeTestCase):
+	pass
